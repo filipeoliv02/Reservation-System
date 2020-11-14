9876e480d5f25e15c4d1da48e6354ef308fcec96
filipe <filipeh291@gmail.com>
2020-11-14 20:06:08 +0000

    first commit

---
+++ cmake-build-debug/CMakeFiles/AED1_LP1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AED1_LP1"
+  "AED1_LP1.pdb"
+  "CMakeFiles/AED1_LP1.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AED1_LP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Buenos Dias!\n");
+    return 0;
+}
