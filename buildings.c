c62fb0abdbdce3479032db6ea99ea21bc0592974
Rui Rodrigues | EGITRON <rui.rodrigues@egitron.pt>
2020-12-31 12:15:37 +0000

    [TASK] Creat methot to List

--- buildings.c
+++ buildings.c
@@ -48,3 +48,57 @@ void write_buildings_file(char *fname) {
     fprintf(bfile, "%d,%s,%lf,%lf,%s,%lf\n", build.id, build.name,
             build.latitude, build.longitude, build.address, build.priceperday);
 }
+
+void read_buildings_to_List(char *fname) {
+    FILE *bfile = fopen(fname, "r");
+    if (bfile == NULL) {
+        printf("File not Found - 404 read_buildings_file\n");
+        exit(1);
+    }
+
+    BUILDING build;
+
+    BUILDING *L=NULL,*aux;
+
+    while (!feof(bfile)) {
+        if(L==NULL){
+            build.name = malloc(sizeof(build.name));
+            build.address = malloc(sizeof(build.address));
+            L=(BUILDING*)malloc (1*sizeof(BUILDING));
+
+            fscanf(bfile, "%d[^,]", L->id=build.id);
+            fscanf(bfile, ",%[^,]s", L->name=build.name);
+            fscanf(bfile, ",%lf[^,]", L->latitude=build.latitude);
+            fscanf(bfile, ",%lf[^,]", L->longitude=build.longitude);
+            fscanf(bfile, ",%[^,]s", L->address=build.address);
+            fscanf(bfile, ",%lf[^\n]", L->priceperday=build.priceperday);
+            aux=L;
+        }
+
+        else {
+            aux->next=(BUILDING*)malloc (1*sizeof(BUILDING));
+            aux=aux->next;
+            strcpy(aux->name,fscanf(bfile, ",%[^,]s", L->name=build.name));
+            aux->id=fscanf(bfile, "%d[^,]", L->id=&build.id);
+            aux->next=NULL;
+        }
+    }
+
+
+
+    build.name = malloc(sizeof(build.name));
+    build.address = malloc(sizeof(build.address));
+
+    while (!feof(bfile)) {
+        fscanf(bfile, "%d[^,]", &build.id);
+        fscanf(bfile, ",%[^,]s", build.name);
+        fscanf(bfile, ",%lf[^,]", &build.latitude);
+        fscanf(bfile, ",%lf[^,]", &build.longitude);
+        fscanf(bfile, ",%[^,]s", build.address);
+        fscanf(bfile, ",%lf[^\n]", &build.priceperday);
+
+        printf("%d %s %lf %lf %s %lf\n", build.id, build.name, build.latitude,
+               build.longitude, build.address, build.priceperday);
+    }
+    fclose(bfile);
+}
--- main.c
+++ main.c
@@ -10,12 +10,14 @@
  */
 
 int main() {
-    /*read_buildings_file("api_files/edificios.csv");*/
+    read_buildings_file("api_files/edificios.csv");
     /*write_buildings_file("api_files/test2.csv");*/
+    //remove_fromBuildings_file("api_files/test2.csv");
    /*read_events_file("api_files/eventos.csv");*/
     //write_events_file("api_files/test.csv");
     //read_studios_file("api_files/estudios.csv");
-    write_studios_file("api_files/test.csv");
+    //write_studios_file("api_files/test.csv");
+
     return 0;
 }
 
